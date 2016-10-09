5b7983aae1c255d9a97949c3b52c58480db0f114
GrouchyBear <benjaminshlack@gmail.com>
2016-10-10 00:37:22 +0100

    Added a one to many relationship between tickets and categories

--- app/Ticket.php
+++ app/Ticket.php
@@ -9,4 +9,9 @@ class Ticket extends Model
     protected $fillable = [
       'user_id', 'category_id', 'ticket_id', 'title', 'priority', 'message', 'status'
     ];
+
+    public function category()
+    {
+      return $this->belongsTo(Category::class);
+    }
 }
