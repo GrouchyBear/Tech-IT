1e12e3af99fb93bf5e55a6e716aa891aa46e36fa
GrouchyBear <benjaminshlack@gmail.com>
2016-10-10 00:34:26 +0100

    Added ticket and category models and migration files

---
+++ app/Ticket.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ticket extends Model
+{
+    protected $fillable = [
+      'user_id', 'category_id', 'ticket_id', 'title', 'priority', 'message', 'status'
+    ];
+}
