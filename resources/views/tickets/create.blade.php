53f1744b87877c60ec4f72c98c780ea5793459c4
GrouchyBear <benjaminshlack@gmail.com>
2016-10-10 14:15:04 +0100

    Added critical priority

--- resources/views/tickets/create.blade.php
+++ resources/views/tickets/create.blade.php
@@ -56,6 +56,7 @@
                                     <option value="low">Low</option>
                                     <option value="medium">Medium</option>
                                     <option value="high">High</option>
+                                    <option value="critical">Critical</option>
                                 </select>
 
                                 @if ($errors->has('priority'))
