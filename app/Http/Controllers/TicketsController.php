ae1821ccb63eb44c986c9aa699173d7debb5f899
GrouchyBear <benjaminshlack@gmail.com>
2016-10-11 00:10:04 +0100

    removed $mailer function and changed migration table for ticket creation

--- app/Http/Controllers/TicketsController.php
+++ app/Http/Controllers/TicketsController.php
@@ -38,8 +38,6 @@ class TicketsController extends Controller
 
       $ticket->save();
 
-      $mailer->sendTicketInformation(Auth::user(), $ticket);
-
       return redirect()->back()->with("status", "A ticket with ID: #$ticket->ticket_id has been opened");
     }
 }
--- database/migrations/2016_10_09_232805_create_tickets_table.php
+++ database/migrations/2016_10_09_232805_create_tickets_table.php
@@ -14,15 +14,15 @@ class CreateTicketsTable extends Migration
     public function up()
     {
         Schema::create('tickets', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('user_id')->unsigned();
-            $table->integer('category_id')->unsigned();
-            $table->integer('ticket_id')->unsigned();
-            $table->string('title');
-            $table->string('priority');
-            $table->text('message');
-            $table->string('status');
-            $table->timestamps();
+          $table->increments('id');
+          $table->integer('user_id')->unsigned();
+          $table->integer('category_id')->unsigned();
+          $table->string('ticket_id')->unique();
+          $table->string('title');
+          $table->string('priority');
+          $table->text('message');
+          $table->string('status');
+          $table->timestamps();
         });
     }
 
