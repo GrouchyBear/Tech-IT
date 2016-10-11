1daac7d6c2298ba538750f6a18207c7963fdacf7
GrouchyBear <benjaminshlack@gmail.com>
2016-10-11 14:09:00 +0100

    added a menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,6 +54,9 @@
                             <li><a href="{{ url('/login') }}">Login</a></li>
                             <li><a href="{{ url('/register') }}">Register</a></li>
                         @else
+                            <li><a href="{{ url('new_ticket') }}">Submit</a></li>
+                            <li><a href="{{ url('user_tickets') }}">Open Tickets</a></li>
+
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
