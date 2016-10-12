47c0434f0f3559c66b34a43c701bfbc76edd05bc
GrouchyBear <benjaminshlack@gmail.com>
2016-10-12 22:56:07 +0100

    changed some colours

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,8 +13,8 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #2c3e50;
-                color: #ecf0f1;
+                background-color: #22313F;
+                color: #E4F1FE;
                 font-family: 'Raleway', sans-serif;
                 font-weight: 100;
                 height: 100vh;
@@ -50,7 +50,7 @@
             }
 
             .links > a {
-                color: #636b6f;
+                color: #E4F1FE;
                 padding: 0 25px;
                 font-size: 12px;
                 font-weight: 600;
