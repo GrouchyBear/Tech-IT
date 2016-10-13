7a62b10817f7a8114c57fc4744e2a59fba7c409a
GrouchyBear <benjaminshlack@gmail.com>
2016-10-13 01:31:43 +0100

    switched block, and fixed spelling error

--- resources/views/tickets/index.blade.php
+++ resources/views/tickets/index.blade.php
@@ -28,11 +28,11 @@
                             @foreach ($tickets as $ticket)
                                 <tr>
                                     <td>
-                                   @foreach ($categories as $category)
-                                        @if ($category->id === $ticket->category_id)
-                                            {{ $category->name }}
-                                        @endif
-                                    @endforeach
+                                      @foreach ($categories as $category)
+                                          @if ($category->id === $ticket->category_id)
+                                              {{ $category->name }}
+                                          @endif
+                                      @endforeach
                                     </td>
                                     <td>
                                         <a href="{{ url('tickets/'. $ticket->ticket_id) }}">
