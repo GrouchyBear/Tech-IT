1dec26176fcdf969216c219a536d5e02c44f689a
GrouchyBear <benjaminshlack@gmail.com>
2016-10-13 11:42:14 +0100

    added dropdown to admin menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,8 +56,30 @@
                             <li><a href="{{ url('/register') }}">Register</a></li>
 
                         @elseif (Auth::user()->is_admin)
-                        
+
+
+                            <li><a href="{{ url('new_ticket') }}">Submit</a></li>
                             <li><i class="fa fa-admin"></i><a href="{{ url('admin/tickets') }}">Admin</a></li>
+
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu" role="menu">
+                                    <li>
+                                        <a href="{{ url('/logout') }}"
+                                            onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            Logout
+                                        </a>
+
+                                        <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
 
                         @else
 
